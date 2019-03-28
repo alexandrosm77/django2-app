d79153ebfafff151df0b1115fb77a65b370c24e7
Alexandros Mystridis <alexandros.mystridis@voxygen.co.uk>
2019-03-28 18:18:05 +0000

    Add endpoint for creating and authenticating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jzr@2!@ycue!zhijhegiaa!28v9$b&)n()l3k3t33c7439v#_5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
